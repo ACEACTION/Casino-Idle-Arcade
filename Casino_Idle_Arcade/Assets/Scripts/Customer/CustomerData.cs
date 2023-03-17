ab82c4c98ddafacae3f8f37e4e8dd3e9dd8f20ea
mohammad firoozi <72497068+mohammadfiroozi1@users.noreply.github.com>
2023-03-17 21:36:09 +0330

    new assets
    new effects + customer animation ( downloaded from mixamo) imoprted

---
+++ Casino_Idle_Arcade/Assets/Scripts/Customer/CustomerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "NormalCustomer data")]
+public class CustomerData : ScriptableObject
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] int cashAmount;
+    [SerializeField] Transform[] Destinations;
+    
+}
