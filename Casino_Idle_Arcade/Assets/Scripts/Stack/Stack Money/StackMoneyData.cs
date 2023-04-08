26c32e3d3b5f54e88f2072be7d0fa30bb243407f
Rasoul Sharifi <rasoul.sharifi1988@gmail.com>
2023-04-09 02:08:42 +0330

    add stack money data

---
+++ Casino_Idle_Arcade/Assets/Scripts/Stack/Stack Money/StackMoneyData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+[CreateAssetMenu(menuName = "Data/Stack Money Data")]
+public class StackMoneyData : ScriptableObject
+{
+    public int xSize = 5;
+    public int ySize = 5;
+    public int zSize = 5;
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+    public float moneyMoveSpeed;
+    public Ease moneyMoveEase;
+    public float goToPlayerDelay;
+}
