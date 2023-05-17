2145739225c3c187914129d809e6c144eb35841d
Rasoul Sharifi <rasoul.sharifi1988@gmail.com>
2023-05-17 16:45:15 +0330

    change ui money bag

--- Casino_Idle_Arcade/Assets/Scripts/Manager/CasinoManager.cs
+++ Casino_Idle_Arcade/Assets/Scripts/Manager/CasinoManager.cs
@@ -24,7 +24,7 @@ public class CasinoManager : MonoBehaviour
             instance = this;
         }
 
-        GameManager.totalMoney += 10020000;
+        GameManager.totalMoney += 300;
 
         if (isCompleteTutorial)
         {
